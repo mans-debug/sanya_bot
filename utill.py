fba86c8b67a6c085a858ac20b20f738add01d242
Mansur <mansurminnikaev@gmail.com>
2024-08-15 22:40:15 +0300

    remove junk

--- utill.py
+++ utill.py
@@ -23,24 +23,3 @@ def compare(data):
 
     return "\n".join(map(lambda bank_result: f"{bank_result[0]} {bank_result[1]}", result.items()))
 
-
-def create_data(pdf_path):
-    with open(pdf_path, 'rb') as file:
-        pdf_file = PdfReader(file)
-        pdf_info = pdf_file.metadata
-        version = pdf_file.pdf_header
-
-    data = {
-        "size": os.path.getsize(pdf_path) // 1024,
-        "ver": version[5:8],
-        "creator": pdf_info.get('/Creator'),
-        "producer": pdf_info.get('/Producer'),
-    }
-    return compare(data)
-
-
-if __name__ == "__main__":
-    dir_path = "sample_pdf/"
-    pdfs = os.listdir(dir_path)
-    for pdf in pdfs:
-        print(create_data(dir_path + pdf), sep="\n\n")
